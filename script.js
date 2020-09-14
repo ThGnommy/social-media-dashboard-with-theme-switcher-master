f8d8901b783ad1736b3c75349c4def078c639088
ThGnommy <th.brandoli@gmail.com>
2020-09-14 21:47:33 +0200

    responsive

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const toggle = document.getElementById("toggle");
 const toggleContainer = document.getElementById("toggle-container")
 
-const handleDarkMode = () => {
-    toggle.classList.toggle('toggle-button-light')
+const backgroundDark = document.querySelectorAll(".background-dark");
+const backgroundDarkTop = document.querySelectorAll(".background-dark-top");
+const backgroundCard = document.querySelectorAll(".card-bg-dark")
+const textPrimary = document.querySelectorAll(".dark-mode-desaturated-blue");
+const textSecondary = document.querySelectorAll(".dark-theme-text-white");
 
+
+const handleTheme = () => {
+    toggle.classList.toggle('toggle-button-light')
     toggleContainer.classList.toggle('toggle-container-light')
+
+    toggleClass(backgroundDark, 'background-white');
+    toggleClass(backgroundDarkTop, 'background-white');
+    toggleClass(backgroundCard, 'card-bg-white');
+    toggleClass(textPrimary, 'light-mode-text-blue')
+    toggleClass(textSecondary, 'light-theme-text-blue')
 }
 
-toggle.addEventListener('click', handleDarkMode);
+const toggleClass = (array, classString) => {
+    array.forEach(element => {
+        element.classList.toggle(classString)
+    });
+}
+
+const darkTheme = () => {}
+
+const whiteTheme = () => {}
+
+toggle.addEventListener('click', handleTheme);
